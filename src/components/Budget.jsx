5461f40157cf18ab3783660c8f3b19e85675494c
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-03-31 22:29:45 -0500

    tables in progress

--- src/components/Budget.jsx
+++ src/components/Budget.jsx
@@ -1,16 +1,16 @@
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
 
-export default function Budget({ budget }){
+// export default function Budget({ budget }){
 
-    const id = budget.url.split('/')[4]
-    console.log(id)
+//     const id = budget.url.split('/')[4]
+//     console.log(id)
 
-    return (
-        <div>
-            <Link to={`/budget/${id}`}>
-                <h1>{budget.type}</h1>
-                <h1>{budget.amount}</h1>
-            </Link>
-        </div>
-        )
-}
+//     return (
+//         <div>
+//             <Link to={`/budget/${id}`}>
+//                 <h1>{budget.type}</h1>
+//                 <h1>{budget.amount}</h1>
+//             </Link>
+//         </div>
+//         )
+// }
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,21 +1,54 @@
-import Budget from "../components/Budget"
+// import Budget from "../components/Budget"
 import { useLoaderData, Form } from "react-router-dom"
 import Button from '@mui/material/Button';
-import * as React from 'react';
+import React, {useEffect, useState} from 'react'
+// import { createAction } from "../actions";
+// import ColumnTypesGrid from "../components/Table";
+import DataTable from "../components/Table";
+import DoughnutChart from "../components/PieChart";
+import Chart from 'chart.js/auto';
+// import * as React from 'react';
+import TransactionTable from "../components/Table";
 
 // import { BarChart } from '@mui/x-charts/BarChart';
-import PieChartStuff from "../components/PieChart";
-import PieChart from '@mui/x-charts/PieChart';
+// import PieChartStuff from "../components/PieChart";
+// import PieChart from '@mui/x-charts/PieChart';
 
 export default function Index(props) {
-    const allTransactions = useLoaderData()
-    // allTransactions.map((item, i) => <Budget budget={item}/>)
-    console.log(allTransactions[0].amount)
-    allTransactions.map((money, i) => money)
+    // const allTransactions = useLoaderData()
+    // console.log(allTransactions)
+    
+
+
+    const [incomeTransactions, setIncomeTransactions] = useState([])
+    const [expenseTransactions, setExpenseTransactions] = useState([])
+
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const allTransactions = await useLoaderData()
+    //         setTransactions(allTransactions)
+    //         setIncomeTransactions(allTransactions.filter((t) => t.type === "Income"))
+    //         setExpenseTransactions(allTransactions.filter((t) => t.type === "Expense"))
+    //     }
+    //     fetchData()
+    // }, [])
+
+    // const handleSubmit = async (event) => {
+    //     event.preventDefault()
+    //     await createAction()
+    //     const newTransactions = await useLoaderData()
+    //     setTransactions(newTransactions)
+    //     setIncomeTransactions(newTransactions.filter((t) => t.type === "Income"))
+    //     setIncomeTransactions(newTransactions.filter((t) => t.type === "Expense"))
+    // }
+
+    // let incomeTransactions = allTransactions.filter((t) => t.type === "Income")
+    // console.log(incomeTable)
    
 
     return (
         <>
+            {/* <DoughnutChart/> */}
             <h1>Add an Expense</h1>
             <Form action="/create" method="POST">
                 <label htmlFor="type">
@@ -37,10 +70,17 @@ export default function Index(props) {
             </Form>
             
             <br />
-            {allTransactions.map((transaction, i) => <Budget budget={transaction} key={i}/>)}
+
+            <h2>Income Table</h2>
+            <TransactionTable transactions={incomeTransactions} budget="Income"/>
+            {/* {incomeTable} */}
+            <h2>Expense Table</h2>
+            <TransactionTable transactions={expenseTransactions} budget="Expense"/>
+
+                {/* {allTransactions.map((transaction, i) => <Budget budget={transaction} key={i}/>)} */}
 
    
-            <PieChartStuff/>
+
             
         </>
     )
